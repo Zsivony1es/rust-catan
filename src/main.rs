59d1f6ca556b72c6f249fe5ed7dbb47a74dc30ef
Zsivony1es <github.flask852@passmail.com>
2026-09-13 13:24:51 +0200

    Initialize Sentry in project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let _guard = sentry::init(("https://547bc132aefdf46bec7eeff82cfa026e@o4512078782136320.ingest.de.sentry.io/4512078786723920", sentry::ClientOptions {
+    release: sentry::release_name!(),
+    // Capture user IPs and potentially sensitive headers when using HTTP server integrations
+    // see https://docs.sentry.io/platforms/rust/data-management/data-collected for more info
+    send_default_pii: true,
+    ..Default::default()
+    }));
+
+    // Sentry will capture this
+    panic!("Everything is on fire!");
 }
