89923796c2df5af680b0568400539607959f3e9b
Zsivony1es <github.flask852@passmail.com>
2026-09-13 13:32:00 +0200

    Initial corrected Sentry integration commit with builder API

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-    let _guard = sentry::init(("https://547bc132aefdf46bec7eeff82cfa026e@o4512078782136320.ingest.de.sentry.io/4512078786723920", sentry::ClientOptions {
-    release: sentry::release_name!(),
-    // Capture user IPs and potentially sensitive headers when using HTTP server integrations
-    // see https://docs.sentry.io/platforms/rust/data-management/data-collected for more info
-    send_default_pii: true,
-    ..Default::default()
-    }));
+  let _guard = sentry::init((
+    "https://547bc132aefdf46bec7eeff82cfa026e@o4512078782136320.ingest.de.sentry.io/4512078786723920",
+    sentry::ClientOptions::new()
+      .maybe_release(sentry::release_name!())
+      // Capture user IPs and potentially sensitive headers when using HTTP server integrations
+      // see https://docs.sentry.io/platforms/rust/data-management/data-collected for more info
+      .send_default_pii(true),
+  ));
 
-    // Sentry will capture this
-    panic!("Everything is on fire!");
+  println!("Hello, world!");
+  panic!("Everything is on fire!");
 }
